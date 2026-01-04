adcbe91071be4c0585825a7fdf8e00733dc2f123
Kedar Bhagwanrao Baswade <kedar@Kedar.local>
2026-01-05 00:13:40 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,28 +1,25 @@
 import readline from "readline";
 
-
 const rl = readline.createInterface({
-  input: process.stdin,   // terminal se aane wala stream
+  input: process.stdin, // terminal se aane wala stream
   output: process.stdout, // terminal pe output stream
 });
 
+
 //    Question turant print hota hai
 //    Callback TAB chalta hai jab terminal me ENTER dabta hai
+// rl.question("Enter your name: ", (name) => {
+//   console.log("Hello", name);
 
-rl.question("Enter your name: ", (name) => {
-  console.log("Hello", name);
-
-  // prompt set kiya (CLI style)
-  rl.setPrompt("$");
-  rl.prompt(); // terminal me "cmd> " dikhane ke liye
-});
+//   // prompt set kiya (CLI style)
+// });
+rl.setPrompt("$");
+rl.prompt(); // terminal me "cmd> " dikhane ke liye
 
 //    Har baar ENTER dabane pe ye chalega
 
 rl.on("line", (input) => {
-
   console.log(`${input}: command not found`);
-
   if (input === "exit") {
     rl.close(); // readline band
     return;
