b4f1b5d9eefff73c2186bf8b5438e1e8e6e146af
Kedar Bhagwanrao Baswade <kedar@Kedar.local>
2026-01-05 00:37:48 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,39 +1,25 @@
 import readline from "readline";
 
 const rl = readline.createInterface({
-  input: process.stdin, // terminal se aane wala stream
-  output: process.stdout, // terminal pe output stream
+  input: process.stdin,
+  output: process.stdout,
 });
 
-//    Question turant print hota hai
-//    Callback TAB chalta hai jab terminal me ENTER dabta hai
-// rl.question("Enter your name: ", (name) => {
-//   console.log("Hello", name);
-
-//   // prompt set kiya (CLI style)
-// });
-
 rl.setPrompt("$ ");
-rl.prompt(); // terminal me "cmd> " dikhane ke liye
+rl.prompt();
 
 rl.on("line", (input) => {
   if (input === "exit") {
-    rl.close(); // readline band
+    rl.close();
     return;
   }
-  if(input.startsWith(`echo `)){
-    const a=input.slice(5);
-    console.log(a);
+
+  if (input.startsWith("echo ")) {
+    const output = input.slice(5);
+    console.log(output);
+  } else {
+    console.log(`${input}: command not found`);
   }
-  // console.log(`${input}: command not found`);
-  // next input ke liye prompt dubara
 
   rl.prompt();
-
 });
-
-// 4️⃣ CLOSE EVENT
-//    Ye TAB chalega jab rl.close() call hota hai
-// rl.on("close", () => {
-//   console.log("Program ended.");
-// });
--- z.js
+++ z.js
@@ -1,25 +1,42 @@
 import readline from "readline";
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  input: process.stdin, // terminal se aane wala stream
+  output: process.stdout, // terminal pe output stream
 });
+
+//    Question turant print hota hai
+//    Callback TAB chalta hai jab terminal me ENTER dabta hai
+// rl.question("Enter your name: ", (name) => {
+//   console.log("Hello", name);
+
+//   // prompt set kiya (CLI style)
+// });
 
 rl.setPrompt("$ ");
-rl.prompt();
+rl.prompt(); // terminal me "cmd> " dikhane ke liye
 
 rl.on("line", (input) => {
+
   if (input === "exit") {
-    rl.close();
+    rl.close(); // readline band
     return;
   }
-
-  if (input.startsWith("echo ")) {
-    const output = input.slice(5);
-    console.log(output);
-  } else {
+  else if(input.startsWith(`echo `)){
+    const a=input.slice(5);
+    console.log(a);
+  }
+  else{
     console.log(`${input}: command not found`);
   }
+  // next input ke liye prompt dubara
 
   rl.prompt();
+
 });
+
+// 4️⃣ CLOSE EVENT
+//    Ye TAB chalega jab rl.close() call hota hai
+// rl.on("close", () => {
+//   console.log("Program ended.");
+// });  
