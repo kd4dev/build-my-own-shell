1d1744d19fce89cda186383d4ee705508122014b
Kedar Bhagwanrao Baswade <kedar@Kedar.local>
2026-01-05 23:06:37 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -57,8 +57,7 @@ rl.on("line", (input) => {
 
     } 
 
-    else {
-
+    else{
         const parts = input.trim().split(" ");
         const command = parts[0];
         const args = parts.slice(1);
@@ -67,18 +66,18 @@ rl.on("line", (input) => {
         for (const folder of folders) { 
             const fullPath = path.join(folder, command);
             try {
-               fs.accessSync(fullPath, fs.constants.X_OK);
-               foundPath = fullPath; 
-               break; 
-            } 
-            catch (err) {}
+                fs.accessSync(fullPath, fs.constants.X_OK);
+                foundPath=fullPath;
+                break; 
+            }
+             catch () {}
         }
-
-        if (foundPath) {
-            const result = spawnSync(command, args, { stdio: 'inherit' });   
+        if(foundPath) {
+            const output = execSync(input, { encoding: 'utf-8' });
+            console.log(output);
         }
         else console.log(`${input}: command not found`);
-    }
+    } 
 
     rl.prompt();
 
